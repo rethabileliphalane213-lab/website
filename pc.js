097757eeab39aafd0ce9e5a2788faae9c5f2b772
Rethabile <rethabileliphalane213@gmail.com>
2026-04-19 09:10:18 +0000

    front end complete

--- pc.js
+++ pc.js
@@ -27,11 +27,13 @@ if(array[i]*fontSize> leftCanvas.height && Math.random()>.95){
     array[i]=0
 }
 }
+upMatrix()
 }
-setInterval(matrix,50)
+
 
 
 const rightCanvas=document.getElementById("right-canvas")
+
 rightCanvas.width=window.innerWidth/4
 rightCanvas.height=window.innerHeight
 
@@ -41,17 +43,24 @@ for(let i=0;i<coloumn;i++){
     array2[i]=rightCanvas.height/fontSize
 }
 
+const ctxRight = rightCanvas.getContext("2d")
+
 function upMatrix(){
-    ctx.fillStyle="rgba(0,0,0,0.05)"
-    ctx.fillRect(0,0,rightCanvas.width,rightCanvas.height)
-    /*const random=Math.floor(Math.random()*displayName.length)*/
-for(let i=0;i<array.length;i++){
-const text=displayName
-ctx.fillStyle="#0f0"
-ctx.fillText(text,i*fontSize,array[i]*fontSize)
-array[i]--
-if(array[i]*fontSize> rightCanvas.height && Math.random()>.95){
-    array[i]=0
+    ctxRight.fillStyle = "rgba(0,0,0,0.05)"
+    ctxRight.fillRect(0, 0, rightCanvas.width, rightCanvas.height)
+
+    for(let i=0; i<array2.length; i++){
+        const random = Math.floor(Math.random() * displayName.length)
+        const text = displayName[random]
+
+        ctxRight.fillStyle = "#0f0"
+        ctxRight.fillText(text, i*fontSize, array2[i]*fontSize)
+
+        array2[i]--
+        if(array2[i]*fontSize < 0 && Math.random() > 0.95){
+            array2[i] = rightCanvas.height / fontSize
+        }
+    }
 }
-}
-}
+
+setInterval(matrix,50)
